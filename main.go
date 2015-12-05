e70ef2fc754543ee0a894e043b947d90eeac6904
martinrusev <martin@amon.cx>
2015-12-05 04:35:33 +0200

    Process fixes

--- main.go
+++ main.go
@@ -1,44 +1,155 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
 
-	"github.com/martinrusev/amonagent/collectors"
 	"github.com/martinrusev/amonagent/logging"
 	"github.com/martinrusev/amonagent/util"
-	"github.com/shirou/gopsutil/host"
-	"github.com/shirou/gopsutil/load"
 	"github.com/shirou/gopsutil/mem"
-	"github.com/shirou/gopsutil/net"
 )
 
 // AmonAgentLogger for the main file
 var AmonAgentLogger = logging.GetLogger("amonagent")
 
-func main() {
+func (p ProcessStruct) String() string {
+	s, _ := json.Marshal(p)
+	return string(s)
+}
+
+type ProcessStruct struct {
+	CPU     float64 `json:"cpu"`
+	Memory  float64 `json:"memory_mb"`
+	KBRead  float64 `json:"kb_read"`
+	KBWrite float64 `json:"kb_write"`
+	Command string  `json:"command"`
+}
+
+type ProcessesList []*ProcessStruct
+
+func Processes() {
+	c1, _ := exec.Command("pidstat", "-ruhtd").Output()
+
+	ps := ProcessesList{}
+
 	v, _ := mem.VirtualMemory()
-	fmt.Println(v)
+	memoryTotalMB, _ := util.ConvertBytesTo(float64(v.Total), "mb")
 
-	n, _ := net.NetIOCounters(true)
-	fmt.Println(n)
+	// Find header and ignore
+	headerRegex, _ := regexp.Compile("d+")
 
-	memoryTotalMB, _ := util.ToMegabytes(v.Total)
-	fmt.Println(memoryTotalMB)
+	pidstatOutput := string(c1)
+	pidstatLines := strings.Split(pidstatOutput, "\n")
+	// var err error
+	for _, processLine := range pidstatLines {
 
-	l, _ := load.LoadAvg()
-	fmt.Println(l)
+		if len(headerRegex.FindString(processLine)) == 0 {
+			processData := strings.Fields(processLine)
 
-	fmt.Println(v.Total)
-	s, _ := host.HostInfo()
-	fmt.Println(s)
+			// Helper
+			// Time(0)   UID(1)      TGID(2)       TID(3)
+			// %usr{4} %system{5}  %guest{6}    %CPU{7}   CPU{8}
+			// minflt/s{9}  majflt/s{10}     VSZ{11}    RSS{12}
+			// %MEM{13}   kB_rd/s{14}   kB_wr/s{15} kB_ccwr/s{16}  Command{17}
+			if len(processData) == 18 {
+				pid, masterthreadID, cpuPercent, memoryPercent, processName := processData[2], processData[3], processData[7], processData[13], processData[17]
 
-	d, _ := collectors.DiskSpace()
-	fmt.Println(d)
+				masterthreadIDtoINT, _ := strconv.Atoi(masterthreadID)
+				cpuPercenttoINT, _ := strconv.ParseFloat(cpuPercent, 64)
+				memoryPercenttoINT, _ := strconv.ParseFloat(memoryPercent, 64)
+
+				if masterthreadIDtoINT == 0 {
+
+					ioFile, e := ioutil.ReadFile("/proc/" + pid + "/io")
+					if e != nil {
+						continue
+					}
+					var io []string
+					for _, line := range strings.Split(string(ioFile), "\n") {
+						f := strings.Fields(line)
+						if len(f) == 2 {
+							io = append(io, f[1])
+						}
+					}
+
+					var processMemoryMB = util.FloatDecimalPoint(memoryTotalMB/100*memoryPercenttoINT, 2)
+
+					ReadBytesInt, _ := strconv.Atoi(io[4])
+					ReadBytesFloat := float64(ReadBytesInt)
+					processReadKB, _ := util.ConvertBytesTo(ReadBytesFloat, "kb")
+
+					WriteBytesInt, _ := strconv.Atoi(io[5])
+					WriteBytesFloat := float64(WriteBytesInt)
+					processWriteKB, _ := util.ConvertBytesTo(WriteBytesFloat, "kb")
+
+					c := ProcessStruct{
+						CPU:     cpuPercenttoINT,
+						Memory:  processMemoryMB,
+						Command: processName,
+						KBRead:  processReadKB,
+						KBWrite: processWriteKB,
+					}
+
+					append(*ps, c)
+					fmt.Println(c)
+
+				}
+
+			}
+
+		}
+
+	}
+
+	fmt.Print(ps)
+}
+func main() {
+
+	// lps, _ := getLinuxProccesses()
+	//
+	// for _, w := range lps {
+	// 	p, err := process.NewProcess(int32(w.Pid))
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	//
+	// 	fmt.Println(p.IOCounters())
+	// 	duration := time.Duration(1000) * time.Microsecond
+	// 	fmt.Println(p.CPUPercent(duration))
+	// 	fmt.Println(p.MemoryInfo())
+	//
+	// 	fmt.Println(w.Command)
+	// }
+
+	// v, _ := mem.VirtualMemory()
+	// fmt.Println(v)
+	//
+	// n, _ := net.NetIOCounters(true)
+	// fmt.Println(n)
+	//
+	// memoryTotalMB, _ := util.ToMegabytes(v.Total)
+	// fmt.Println(memoryTotalMB)
+
+	// l, _ := load.LoadAvg()
+	// fmt.Println(l)
+	//
+	// fmt.Println(v.Total)
+	// s, _ := host.HostInfo()
+	// fmt.Println(s)
+	//
+	// d, _ := collectors.DiskSpace()
+	// fmt.Println(d)
 	// for _, volume := range d {
 	// 	fmt.Println(volume)
 	//
 	// }
 
-	collectors.Processes()
+	Processes()
+	// collectors.Processes()
 
 }
--- util/filesize.go
+++ util/filesize.go
@@ -1,19 +1,38 @@
 package util
 
+import "math"
+
 // conversion units
 const (
 	BYTE     = 1.0
 	KILOBYTE = 1024 * BYTE
 	MEGABYTE = 1024 * KILOBYTE
 	GIGABYTE = 1024 * MEGABYTE
-	TERABYTE = 1024 * GIGABYTE
 )
 
-
-// ToMegabytes parses a string formatted by ByteSize as megabytes
-func ToMegabytes(s uint64) (uint64, error) {
-
-	bytes := s / MEGABYTE
+// ConvertBytesTo parses a string formatted by ByteSize as kb
+func ConvertBytesTo(s float64, convert string) (float64, error) {
+	var bytes float64
+	switch convert {
+	case "kb":
+		bytes = s / KILOBYTE
+	case "mb":
+		bytes = s / MEGABYTE
+	case "gb":
+		bytes = s / GIGABYTE
+	default:
+		bytes = s / BYTE
+	}
 
 	return bytes, nil
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+// FloatDecimalPoint parses a float64
+func FloatDecimalPoint(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
 }
