1fff0c834ff10573b0cca4055a7d6963731ead06
martinrusev <martin@amon.cx>
2015-12-07 10:33:26 +0200

    CPU usage collector - working

--- collectors/cpu.go
+++ collectors/cpu.go
@@ -1,7 +1,90 @@
 package collectors
 
-import "github.com/martinrusev/amonagent/logging"
+import (
+	"encoding/json"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/martinrusev/amonagent/logging"
+)
 
 var cpuLogger = logging.GetLogger("amonagent.cpu")
 
 //{'iowait': '0.00', 'system': '0.50', 'idle': '98.50', 'user': '1.00', 'steal': '0.00', 'nice': '0.00'}
+// the header values are %idle, %wait
+var headerRE = regexp.MustCompile(`([%][a-zA-Z0-9]+)[\s+]?`)
+var valueRE = regexp.MustCompile(`\d+[\.,]\d+`)
+
+func (p CPUUsageStruct) String() string {
+	s, _ := json.Marshal(p)
+	return string(s)
+}
+
+// CPUUsageStruct - returns CPU usage stats
+type CPUUsageStruct struct {
+	User   float64 `json:"user"`
+	Idle   float64 `json:"idle"`
+	Nice   float64 `json:"nice"`
+	Steal  float64 `json:"steal"`
+	System float64 `json:"system"`
+}
+
+// CPUUsage - return a map with CPU usage stats
+func CPUUsage() CPUUsageStruct {
+	c1, _ := exec.Command("sar", "1", "1").Output()
+
+	sarOutput := string(c1)
+	sarLines := strings.Split(sarOutput, "\n")
+	header := []string{}
+	values := []float64{}
+
+	result := make(map[string]float64)
+
+	// Get the header
+	for _, line := range sarLines {
+		// Replace the regex with something faster
+		matches := headerRE.FindAllStringSubmatch(line, -1)
+
+		for _, m := range matches {
+			if len(m) == 2 {
+				result := strings.Replace(m[0], "%", "", -1) // replace % in idle (%idle)
+				result = strings.Trim(result, " ")           // remove white space
+				header = append(header, result)
+			}
+		}
+	}
+
+	// Get values
+	for _, line := range sarLines {
+		if strings.Contains(line, "Average") {
+			matches := valueRE.FindAllStringSubmatch(line, -1)
+			for _, m := range matches {
+				if len(m) == 1 {
+					valueFloat, _ := strconv.ParseFloat(m[0], 64)
+					values = append(values, valueFloat)
+				}
+
+			}
+		}
+
+	}
+
+	if len(header) == len(values) {
+		for i := range header {
+			result[header[i]] = values[i]
+		}
+
+	}
+
+	c := CPUUsageStruct{
+		User:   result["user"],
+		Idle:   result["idle"],
+		Nice:   result["nice"],
+		Steal:  result["steal"],
+		System: result["system"],
+	}
+
+	return c
+}
--- main.go
+++ main.go
@@ -1,74 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os/exec"
-	"regexp"
-	"strconv"
-	"strings"
 
+	"github.com/martinrusev/amonagent/collectors"
 	"github.com/martinrusev/amonagent/logging"
 )
 
 // AmonAgentLogger for the main file
 var AmonAgentLogger = logging.GetLogger("amonagent")
 
-// the header values are %idle, %wait
-var headerRE = regexp.MustCompile(`([%][a-zA-Z0-9]+)[\s+]?`)
-var valueRE = regexp.MustCompile(`\d+[\.,]\d+`)
-
 // Just for testing
 func main() {
-	c1, _ := exec.Command("sar", "1", "1").Output()
 
-	sarOutput := string(c1)
-	sarLines := strings.Split(sarOutput, "\n")
-	header := []string{}
-	values := []float64{}
-
-	// var result map[string]float64
-
-	result := make(map[string]float64)
-
-	// Get the header
-	for _, line := range sarLines {
-		// Replace the regex with something faster
-		matches := headerRE.FindAllStringSubmatch(line, -1)
-
-		for _, m := range matches {
-			if len(m) == 2 {
-				result := strings.Replace(m[0], "%", "", -1) // replace % in idle (%idle)
-				header = append(header, result)
-			}
-		}
-	}
-
-	// Get values
-	for _, line := range sarLines {
-		if strings.Contains(line, "Average") {
-			matches := valueRE.FindAllStringSubmatch(line, -1)
-			for _, m := range matches {
-				if len(m) == 1 {
-					valueFloat, _ := strconv.ParseFloat(m[0], 64)
-					values = append(values, valueFloat)
-				}
-
-			}
-		}
-
-	}
-
-	if len(header) == len(values) {
-		for i := range header {
-			result[header[i]] = values[i]
-		}
-
-	}
-
-	jsonString, _ := json.Marshal(result)
-
-	fmt.Println(string(jsonString))
+	cpu := collectors.CPUUsage()
+	fmt.Println(cpu)
 	// if err != nil {
 	// 	return fmt.Errorf("error getting CPU info: %s", err)
 	// }
