32280d7d45c333171099ec15add7ddf5be09655f
martinrusev <martin@amon.cx>
2015-12-10 09:46:02 +0200

    Update remote

--- remote/remote.go
+++ remote/remote.go
@@ -3,18 +3,18 @@ package remote
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/martinrusev/amonagent/core"
-	"github.com/prometheus/common/log"
 )
 
 // DefaultTimeOut - 10 seconds
 var DefaultTimeOut = 10 * time.Second
 
 // SendData - XXX
-func SendData(data interface{}) {
+func SendData(data interface{}) error {
 	settings := core.Settings()
 	url := settings.AmonInstance + "/api/system/v2/?api_key=" + settings.APIKey
 
@@ -26,8 +26,14 @@ func SendData(data interface{}) {
 	client := &http.Client{Timeout: DefaultTimeOut}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Error("Can't connect to the Amon API on %s" + url)
+		return fmt.Errorf("Can't connect to the Amon API on %s\n", err.Error())
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 209 {
+		return fmt.Errorf("received bad status code, %d\n", resp.StatusCode)
+	}
+
+	return nil
 
 }
