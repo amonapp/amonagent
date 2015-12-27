bc528927370609fdb574198a28064e141ef81db9
martinrusev <martin@amon.cx>
2015-12-27 23:48:42 +0200

    Get all enabled plugins

--- plugins/registry.go
+++ plugins/registry.go
@@ -1,1 +1,35 @@
 package plugins
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Config - XXX
+type Config struct {
+	Path string
+	Name string
+}
+
+// ConfigPath - XXX
+const ConfigPath = "/etc/amonagent/plugins-enabled"
+
+// GetAllEnabledPlugins - XXX
+func GetAllEnabledPlugins() ([]Config, error) {
+	fileList := []Config{}
+	filepath.Walk(ConfigPath, func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() {
+			// Only files ending with .conf
+			fileName := strings.Split(f.Name(), ".conf")
+			if len(fileName) == 2 {
+				f := Config{Path: path, Name: fileName[0]}
+				fileList = append(fileList, f)
+			}
+
+		}
+		return nil
+	})
+
+	return fileList, nil
+}
